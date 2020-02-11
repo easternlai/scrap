fc6972bb1245fb1510775149bbdda8bbc20a8757
Eastern Lai <easternlai@hotmail.com>
2020-02-10 23:11:20 -0800

    heroku fix

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var db = require("./models");
 
 var PORT = 3006;
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 var app = express();
 
 app.use(express.urlencoded({extended: true}));
@@ -18,7 +20,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/scrap", {useNewUrlParser: true});
+mongoose.connect(MONGODB_URI);
 
 require("./routes/html-routes.js")(app);
 
